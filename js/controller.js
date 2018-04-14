f917cec7991d1cfeeaddf1a31dbd7b0f271d8d7e
CrumrineCoder <crumrinecoding@gmail.com>
2018-04-14 16:15:36 -0400

    Images added and styized

--- js/controller.js
+++ js/controller.js
@@ -5,6 +5,7 @@ var swiper = new Swiper('.swiper-container', {
         delay: 2500,
         disableOnInteraction: false,
     },
+   
   /*  pagination: {
         el: '.swiper-pagination',
         clickable: true,
